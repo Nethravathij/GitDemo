1eb5e3b1b75388bb2d03791ee4fc618292a42384
Nethravathij <nethravathij695@gmail.com>
2021-01-04 21:20:11 +0530

    architect asian development

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -14,5 +14,27 @@ public class App
         System.out.println("Hello world3");
         System.out.println("Hello world89");
         System.out.println("Hello worldywqu");
+        System.out.println( "Hello !" );
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 89");
+        System.out.println("Hello ywqu");
+    }
+    
+    public  void duplicate( )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world89");
+        System.out.println("Hello worldywqu");
+        System.out.println( "Hello !" );
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 89");
+        System.out.println("Hello ywqu");
     }
 }
