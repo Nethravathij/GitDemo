99cc4789e8f16a7c6fa0cd5e35ad778ca9b5fe75
Nethravathij <nethravathij695@gmail.com>
2021-01-04 21:28:19 +0530

    american development team

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -37,4 +37,19 @@ public class App
         System.out.println("Hello 89");
         System.out.println("Hello ywqu");
     }
+    public  void duplicateTest( )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world89");
+        System.out.println("Hello worldywqu");
+        System.out.println( "Hello !" );
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 89");
+        System.out.println("Hello ywqu");
+    }
 }
